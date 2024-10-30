224bca124136ea596418bef03ad964729ebca4bf
tauheed0110 <tauheed0110@gmail.com>
2024-10-30 15:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const byteSize = (str) => {
-  // write your code here
-	return str.length;
+   const encoder = new TextEncoder();
+    
+    // Encode the string and get the byte length
+    const byteArray = encoder.encode(str);
+    
+    // Return the length of the byte array
+    return byteArray.length;
 };
 
 // Do not change the code below
